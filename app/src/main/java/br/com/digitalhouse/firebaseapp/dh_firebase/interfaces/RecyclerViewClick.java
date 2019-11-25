4db3cc56066bb159de896f0ff7f5209850df1ec0
ArielOliveiraS <gildaariel@outlook.com>
2019-11-25 16:32:34 -0300

    autenticação email/senha e login com facebook

---
+++ app/src/main/java/br/com/digitalhouse/firebaseapp/dh_firebase/interfaces/RecyclerViewClick.java
@@ -0,0 +1,8 @@
+package br.com.digitalhouse.firebaseapp.dh_firebase.interfaces;
+
+import br.com.digitalhouse.firebaseapp.dh_firebase.model.Result;
+
+public interface RecyclerViewClick {
+
+    void clickListener(Result result);
+}
